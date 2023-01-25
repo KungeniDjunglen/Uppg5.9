0fc28ed9c6e22916a9154dcda9bbd5c30e9ef1cf
Admin <Admin@TEB123001>
2023-01-25 12:09:03 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Brogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till personinmatningssystemet. skriv upp till tusen namn");
+            string svar = "j";
+            string[] namn = new string[1000];
+            int x = 0;
+            while (svar == "j")
+            {
+                Console.Write("Skriv ett namn: ");
+                namn[x] = Console.ReadLine();
+                Console.Write("Vill du mata in ett till namn j/n ? ");
+                svar = Console.ReadLine();
+                x++;
+            }
+            Console.Write("Sök upp ett namn bland de du nyss skrev ? ");
+            string NameSearch = Console.ReadLine();
+            int y = 0;
+            foreach(string s in namn)
+            {
+                if (s == NameSearch)
+                {
+                    y++;
+                }
+            }
+            Console.WriteLine("Namnet: " + NameSearch + " Förekomm: " + y + " Gånger");
+
+        }
+    }
+}
